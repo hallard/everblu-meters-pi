237a3717042a61431743bf572a5f06ab1882c426
Charles <hallard04@free.fr>
2023-07-14 11:21:38 +0200

    added scan and some display

--- everblu_meters.c
+++ everblu_meters.c
@@ -1,16 +1,16 @@
  /*  the radian_trx SW shall not be distributed  nor used for commercial product*/
  /*  it is exposed just to demonstrate CC1101 capability to reader water meter indexes */
 
-#define METER_YEAR              16
-#define METER_SERIAL            123456
+#define METER_YEAR              22
+#define METER_SERIAL            828979
 
-#define MQTT_HOST "localhost"
+#define MQTT_HOST "192.168.1.8"
 #define MQTT_PORT  1883
-#define MQTT_USER "homeassistant"
-#define MQTT_PASS "PASS"
+#define MQTT_USER ""
+#define MQTT_PASS ""
 
 
-#define MQTT_KEEP_ALIVE 60
+#define MQTT_KEEP_ALIVE 300
 #define MQTT_MSG_MAX_SIZE  512
 
 
@@ -60,14 +60,20 @@ void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const ch
 
 }
 
+void printMeterData(struct tmeter_data * data)
+{
+    printf("Liters  : %d\r\n", data->liters);
+    printf("Battery : %d months\r\n ", data->battery_left);
+    printf("Counter : %d\r\n", data->reads_counter);
+    printf("Rzding  : from %d to %d\r\n ", data->time_start, data->time_end);
+}
+
 
 void IO_init(void)
 {
 	wiringPiSetup();
 	pinMode (GDO2, INPUT);
 	pinMode (GDO0, INPUT);           
-
-	cc1101_init();
 }
 
 
@@ -78,9 +84,12 @@ int main(int argc, char *argv[])
 	char buff[MQTT_MSG_MAX_SIZE];
 	char meter_id[12];
 	char mqtt_topic[64];
+	//float frequency = 0.0f; // Set to 0 to scan
+	float frequency = 433.8200f; // Set your frequency
+	float f_start = 0.0f;
+    float f_end = 0.0f;
 
 	sprintf(meter_id, "%i_%i", METER_YEAR, METER_SERIAL);
-	
 
 	mosquitto_lib_init();
 	mosq = mosquitto_new(NULL, true, NULL);
@@ -112,18 +121,69 @@ int main(int argc, char *argv[])
 	}
 
 	IO_init();
-	meter_data = get_meter_data();
 
-	sprintf(buff, "%d", meter_data.liters);
-	sprintf(mqtt_topic, "homeassistant/sensor/cyblemeter_%s/state", meter_id);
+	if (!cc1101_init(433.8200f, true)) {
+		printf("\r\nCC1101 Not found, check wiring!\r\n");
+	} else {
+		printf("\r\n");
+		if (frequency == 0.0f) {
+			// Scan al frequencies and blink Blue Led
+			frequency = 433.8200f;
+			float fdev = 0.0000f;
+			while (frequency >= 433.7600f && frequency <= 433.8900f ) {
+				frequency = 433.8200f + fdev;
+				printf("Test frequency : %.4fMHz => ", frequency);
+				fflush(stdout);
+				cc1101_init(frequency, false);
+				meter_data = get_meter_data();
+				// Got datas ?
+				if (meter_data.reads_counter != 0 || meter_data.liters != 0) {
+					printMeterData(&meter_data);
+				} else {
+					printf("No answer\r\n");
+				}
 
-	printf("Liters: %i\n", meter_data.liters);
-	mosquitto_publish(mosq, NULL, mqtt_topic, strlen(buff),buff,0,false);
+				frequency = 433.8200f - fdev;
+				printf("Test frequency : %.4fMHz => ", frequency);
+				fflush(stdout);
+				cc1101_init(frequency, false);
+				meter_data = get_meter_data();
+				// Got datas ?
+				if (meter_data.reads_counter != 0 || meter_data.liters != 0) {
+					printMeterData(&meter_data);
+				} else {
+					printf("No answer\r\n");
+				}
+				fdev = fdev + 0.0005f;
+			}
 
+			// Found frequency Range, setup in the middle
+			if (f_start || f_end) {
+				printf( "Working from %.4fMHz to %.4fMhz\r\n", f_start, f_end);
+				frequency = (f_end - f_start) / 2;
+				frequency += f_start ;
+			} else {
+				printf("Not found a working Frequency!\r\n");
+				frequency = 0.0f;
+			}
+		}
+
+		if (frequency != 0.0f) {
+			cc1101_init(frequency, true);
+			meter_data = get_meter_data();
+		
+			// Got datas ?
+			if (meter_data.reads_counter != 0 || meter_data.liters != 0) {
+				printMeterData(&meter_data);
+				sprintf(buff, "%d", meter_data.liters);
+				sprintf(mqtt_topic, "homeassistant/sensor/cyblemeter_%s/state", meter_id);
+				mosquitto_publish(mosq, NULL, mqtt_topic, strlen(buff),buff,0,false);
+			}
+		}
+	}
 
 	mosquitto_destroy(mosq);
 	mosquitto_lib_cleanup();
-
 
 	return 0;
 }
