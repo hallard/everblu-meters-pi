d13fb1e73663fccc3a443627af3e14f0370d16bc
Charles <hallard04@free.fr>
2023-07-14 13:35:28 +0200

    cosmetic indentation

--- wiringPi.h
+++ wiringPi.h
@@ -36,36 +36,36 @@
 
 // wiringPi modes
 
-#define	WPI_MODE_PINS		 0
-#define	WPI_MODE_GPIO		 1
-#define	WPI_MODE_GPIO_SYS	 2
-#define	WPI_MODE_PHYS		 3
-#define	WPI_MODE_PIFACE		 4
+#define	WPI_MODE_PINS		    0
+#define	WPI_MODE_GPIO		    1
+#define	WPI_MODE_GPIO_SYS	    2
+#define	WPI_MODE_PHYS		    3
+#define	WPI_MODE_PIFACE		    4
 #define	WPI_MODE_UNINITIALISED	-1
 
 // Pin modes
 
-#define	INPUT			 0
-#define	OUTPUT			 1
-#define	PWM_OUTPUT		 2
-#define	GPIO_CLOCK		 3
-#define	SOFT_PWM_OUTPUT		 4
-#define	SOFT_TONE_OUTPUT	 5
-#define	PWM_TONE_OUTPUT		 6
+#define	INPUT			    0
+#define	OUTPUT			    1
+#define	PWM_OUTPUT		    2
+#define	GPIO_CLOCK		    3
+#define	SOFT_PWM_OUTPUT     4
+#define	SOFT_TONE_OUTPUT	5
+#define	PWM_TONE_OUTPUT		6
 
-#define	LOW			 0
-#define	HIGH			 1
+#define	LOW     0
+#define	HIGH	1
 
 // Pull up/down/none
 
-#define	PUD_OFF			 0
-#define	PUD_DOWN		 1
-#define	PUD_UP			 2
+#define	PUD_OFF	 0
+#define	PUD_DOWN 1
+#define	PUD_UP	 2
 
 // PWM
 
-#define	PWM_MODE_MS		0
-#define	PWM_MODE_BAL		1
+#define	PWM_MODE_MS	    0
+#define	PWM_MODE_BAL    1
 
 // Interrupt levels
 
@@ -86,12 +86,12 @@
 #define	PI_MODEL_CM		6
 #define	PI_MODEL_07		7
 #define	PI_MODEL_3		8
-#define	PI_MODEL_ZERO		9
+#define	PI_MODEL_ZERO	9
 
-#define	PI_VERSION_1		0
-#define	PI_VERSION_1_1		1
-#define	PI_VERSION_1_2		2
-#define	PI_VERSION_2		3
+#define	PI_VERSION_1	0
+#define	PI_VERSION_1_1	1
+#define	PI_VERSION_1_2	2
+#define	PI_VERSION_2	3
 
 #define	PI_MAKER_SONY		0
 #define	PI_MAKER_EGOMAN		1
